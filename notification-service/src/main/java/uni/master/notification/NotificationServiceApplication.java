291efc729c7ece33380051d3414b47330a634d38
Lukas <itzzviruz@hotmail.com>
2018-03-17 19:05:35 +0100

    Gateway API complete

--- notification-service/src/main/java/uni/master/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/uni/master/notification/NotificationServiceApplication.java
@@ -1,22 +1,23 @@
-package uni.master.eureka;
+package uni.master.notification;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class EurekaServerApplication {
+public class NotificationServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(NotificationServiceApplication.class, args);
     }
 }
 
 @RefreshScope
 @RestController
-class EurekaRestController {
+class NotificationRestController {
     @Value("${parent-message:zkodu}")
     private String message;
 
