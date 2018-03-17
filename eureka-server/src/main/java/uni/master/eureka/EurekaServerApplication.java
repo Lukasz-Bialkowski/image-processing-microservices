838f1a4fa0f1524105c2d98daa3268b604393acc
Lukas <itzzviruz@hotmail.com>
2018-03-17 16:05:59 +0100

    Notification service added

---
+++ eureka-server/src/main/java/uni/master/eureka/EurekaServerApplication.java
@@ -0,0 +1,27 @@
+package uni.master.eureka;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
+
+@RefreshScope
+@RestController
+class EurekaRestController {
+    @Value("${moj-message:zkodu}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
