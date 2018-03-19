278e0aaf73766d32df9f519fa16212c1955d3d37
Lukas <itzzviruz@hotmail.com>
2018-03-19 06:42:11 +0100

    New yml conf

--- notification-service/src/main/java/uni/master/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/uni/master/notification/NotificationServiceApplication.java
@@ -2,12 +2,14 @@ package uni.master.notification;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
