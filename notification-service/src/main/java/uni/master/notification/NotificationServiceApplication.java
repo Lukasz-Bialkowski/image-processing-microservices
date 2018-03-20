87891228750d1efbb20bdb549e758f23b6df2289
Lukasz Bialkowski <itzzviruz@hotmail.com>
2018-03-20 16:41:11 +0100

    asdasd

--- notification-service/src/main/java/uni/master/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/uni/master/notification/NotificationServiceApplication.java
@@ -1,17 +1,18 @@
 package uni.master.notification;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrixDashboard
+@EnableHystrix
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
@@ -28,5 +29,15 @@ class NotificationRestController {
     @RequestMapping("/message")
     String getMessage() {
         return this.message;
+    }
+
+    @HystrixCommand(defaultFallback = "getDefaultUser")
+    @RequestMapping("/user")
+    String getUser() {
+        return "User taki";
+    }
+
+    String getDefaultUser() {
+        return "Nie ma usera";
     }
 }
