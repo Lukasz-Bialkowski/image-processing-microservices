dd1c8f8aa528b58ef1639276c8c9e93adc969b61
Lukas <itzzviruz@hotmail.com>
2018-05-12 00:07:29 +0200

    write to db as soon as app starts

--- worker-service/src/main/java/uni/master/WorkerServiceApplication.java
+++ worker-service/src/main/java/uni/master/WorkerServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+//import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,10 +15,10 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 @EnableScheduling
 public class WorkerServiceApplication {
 
-    @Bean
-    public TaskScheduler taskScheduler() {
-        return new ConcurrentTaskScheduler(); //single threaded by default
-    }
+//	@Bean
+//	public TaskScheduler taskScheduler() {
+//		return new ConcurrentTaskScheduler(); //single threaded by default
+//	}
 
     public static void main(String[] args) {
         SpringApplication.run(WorkerServiceApplication.class, args);
